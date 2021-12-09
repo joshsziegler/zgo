b8fe21d400e85473754f3cb5241fc461d26b9a97
Josh Ziegler <josh.s.ziegler@gmail.com>
2021-12-09 11:38:45 -0500

    BUGFIX: imports were missing

--- pkg/log/log.go
+++ pkg/log/log.go
@@ -1,14 +1,14 @@
-// log provides opinionated, simplified defaults. 
+// log provides opinionated, simplified defaults.
 //
-// - Two logging modes: prod (default), and debug. 
-//   See the table below for what is output for each mode. 
+// - Two logging modes: prod (default), and debug.
+//   See the table below for what is output for each mode.
 // - To turn on debug logging, set the environment variable 'DEBUG=TRUE'
 // - Date and time are not appended (we assume they are provided by the backend logger such as SystemD).
 // - Log level is output as a single character to make lines shorter and more scannable.
 //
 //
 //        Info  Error  Fatal  Debug
-// Prod   Y     Y      Y      
+// Prod   Y     Y      Y
 // Debug  Y     Y      Y      Y
 //
 //
@@ -34,24 +34,25 @@ package log
 import (
 	"fmt"
 	"log"
+	"os"
+	"strings"
 )
-
 
 var debug bool
 
 func init() {
-    debug := strings.ToLower(strings.Trim(os.Getenv("debug"))) == "true"
+	debug := strings.ToLower(strings.Trim(os.Getenv("debug"))) == "true"
 	log.SetFlags(0) // Disable default flags to hide datetime and level prefixes
 }
 
-// Info logs a normal, informative message. 
+// Info logs a normal, informative message.
 // For information that should not be logged in production, used Debug() instead.
 func Info(args ...interface{}) {
-    log.Print("I: " + fmt.Sprint(args...))
+	log.Print("I: " + fmt.Sprint(args...))
 }
 
 func Infof(format string, args ...interface{}) {
-    log.Print("I: " + fmt.Sprintf(format, args...))
+	log.Print("I: " + fmt.Sprintf(format, args...))
 }
 
 // Debug logs a message that should only be shown only in debug.
@@ -67,7 +68,7 @@ func Debugf(format string, args ...interface{}) {
 	}
 }
 
-// Error logs a message that is a recoverable error. 
+// Error logs a message that is a recoverable error.
 // For unrecoverable errors, use Fatal() instead.
 func Error(args ...interface{}) {
 	log.Print("E: " + fmt.Sprint(args...))
@@ -77,7 +78,7 @@ func Errorf(format string, args ...interface{}) {
 	log.Print("E: " + fmt.Sprintf(format, args...))
 }
 
-// Fatal logs an unrecoverable error. 
+// Fatal logs an unrecoverable error.
 // For recoverable errors, use Error() instead.
 func Fatal(args ...interface{}) {
 	log.Fatal("F: " + fmt.Sprint(args...))
